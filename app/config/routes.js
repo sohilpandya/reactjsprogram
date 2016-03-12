4db284deeab25185234ccf0970846d37adfef46f
sohilpandya <sohilpandya@me.com>
2016-03-12 16:50:18 +0000

    video 5 complete, setting up react routing for more routes, using same component for multiple routes by adding context to the component so that router can push to another endpoint and also pass down query. refactored so that state was handled by a functional component and UI handled by stateless functional component which takes first argument as props. Also talked about proptypes and why they can be usefull, I am in two mids about prop types still...

--- app/config/routes.js
+++ app/config/routes.js
@@ -4,13 +4,17 @@ var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var IndexRoute = ReactRouter.IndexRoute;
 var hashHistory = ReactRouter.hashHistory;
-var Main = require('../components/Main.js')
-var Home = require('../components/Home.js')
+
+var Main = require('../components/Main.js');
+var Home = require('../components/Home.js');
+var PromptContainer = require('../containers/PromptContainer.js')
 
 var routes = (
   <Router history={hashHistory}>
     <Route path='/' component={Main}>
       <IndexRoute component={Home} />
+      <Route path='playerOne' header='Player One'component={PromptContainer} />
+      <Route path='playerTwo/:playerOne' header='Player two' component={PromptContainer} />
     </Route>
   </Router>
 )
